de2f7cc4afe74888a6a40fcc4f505440ff7aa9aa
Simon Spannagel <simon.spannagel@desy.de>
2016-01-27 15:56:22 +0100

    Add header file with some global constants

---
+++ utils/constants.h
@@ -0,0 +1,13 @@
+namespace gblsim {
+
+  // Beam energy in [GeV]
+  double beam_energy = 0.250;
+
+  // Resolution analog planes in [um]
+  double resolution_analog = 30e-3;
+
+  // Resolution digital planes in [um]
+  double resolution_digital = 30e-3;
+
+  
+}
