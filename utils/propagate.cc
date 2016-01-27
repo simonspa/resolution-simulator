7e551f34e3756eb1ac0e1d77519e3c805dc9b9eb
Simon Spannagel <simon.spannagel@desy.de>
2016-01-27 15:11:01 +0100

    Add some tools to propagate the particles through the telescope

---
+++ utils/propagate.cc
@@ -0,0 +1,70 @@
+#include "propagate.h"
+
+TMatrixD gblsim::Jac5(double ds) {
+  /* 
+     straight line, no B-field
+     track = 
+     q/p, x', y', x, y
+     0,   1,  2,  3, 4
+  */
+  TMatrixD jac(5, 5);
+  jac.UnitMatrix();
+  jac[3][1] = ds; // x = xp * ds
+  jac[4][2] = ds; // y = yp * ds
+  return jac;
+}
+
+double gblsim::getTheta(double energy, double radlength) {
+
+  // Return the scattering distribution with Theta according to the Highland forumla
+  // http://pdg.lbl.gov/2015/reviews/rpp2014-rev-passage-particles-matter.pdf (Equation 32.15)
+
+  // Radiation length in [mm], particle energy in [GeV]
+  return (0.0136*sqrt(radlength)/energy*(1+0.038*log(radlength)));
+}
+
+TVectorD gblsim::getScatterer(double energy, double radlength) {
+  TVectorD scat(2);
+
+  double theta = getTheta(energy,radlength);
+  scat[0] = 1.0 / (theta*theta);
+  scat[1] = 1.0 / (theta*theta);
+
+  return scat;
+}
+
+gbl::GblPoint gblsim::getPoint(double dz, double res, TVectorD wscat, bool has_meas) {
+
+  // Propagate:
+  TMatrixD jacPointToPoint = Jac5(dz);
+  gbl::GblPoint point(jacPointToPoint);
+
+  // Add scatterer:
+  TVectorD scat(2);
+  scat.Zero(); // mean is zero
+  point.addScatterer(scat, wscat);
+
+  // Add measurement if requested:
+  if(has_meas) {
+    // measurement = residual
+    TVectorD meas(2);
+    meas.Zero(); // ideal
+
+    // Precision = 1/resolution^2
+    TVectorD measPrec(2);
+    measPrec[0] = 1.0 / res / res;
+    measPrec[1] = 1.0 / res / res;
+
+    TMatrixD proL2m(2,2);
+    proL2m.UnitMatrix();
+  
+    point.addMeasurement(proL2m, meas, measPrec);
+  }
+
+  return point;
+}
+
+gbl::GblPoint gblsim::getPoint(double dz, TVectorD wscat) {
+  // This does not add a measurement - no resultion is given!
+  return getPoint(dz,0.0,wscat,false);
+}
---
+++ utils/propagate.h
@@ -0,0 +1,12 @@
+#include "TMatrixD.h"
+#include "GblTrajectory.h"
+
+namespace gblsim {
+  
+  TMatrixD Jac5(double ds);
+  double getTheta(double energy, double radlength);
+  TVectorD getScatterer(double energy, double radlength);
+  gbl::GblPoint getPoint(double dz, double res, TVectorD wscat, bool has_meas = true);
+  gbl::GblPoint getPoint(double dz, TVectorD wscat);
+
+}
