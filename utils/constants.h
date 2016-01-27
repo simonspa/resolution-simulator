0594dc6a64a3ea02fac07058fcd83ef94ab224c2
Simon Spannagel <simon.spannagel@desy.de>
2016-01-27 19:01:03 +0100

    Use defines

--- utils/constants.h
+++ utils/constants.h
@@ -1,13 +1,13 @@
-namespace gblsim {
+#ifndef CONST_H
+#define CONST_H
 
-  // Beam energy in [GeV]
-  double beam_energy = 0.250;
+// Beam energy in [GeV]
+#define beam_energy 0.250
 
-  // Resolution analog planes in [um]
-  double resolution_analog = 30e-3;
+// Resolution analog planes in [um]
+#define resolution_analog 30e-3
 
-  // Resolution digital planes in [um]
-  double resolution_digital = 30e-3;
+// Resolution digital planes in [um]
+#define resolution_digital 30e-3
 
-  
-}
+#endif /* CONST_H */
--- utils/materials.h
+++ utils/materials.h
@@ -1,26 +1,26 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
 
-namespace gblsim {
+// X0 Si = 21.82/2.33 = 9.365 cm
+#define X0_Si 93.65
 
-  // X0 Si = 21.82/2.33 = 9.365 cm
-  double X0_Si = 93.65; // mm
-
-  // X0 Diamond
-  double X0_Diamond = 122.0; // mm
+// X0 Diamond
+#define X0_Diamond 122.0
   
-  // X0 Al = 24.01/2.70 = 8.89 cm
-  double X0_Al = 88.9; // mm
+// X0 Al = 24.01/2.70 = 8.89 cm
+#define X0_Al 88.9
   
-  // X0 Cu = 12.86/8.96 = 1.435 cm
-  double X0_Cu = 14.35; // mm
+// X0 Cu = 12.86/8.96 = 1.435 cm
+#define X0_Cu 14.35
   
-  // X0 air = 36.66/1.204E-3 = 303.9 m
-  double X0_Air = 304200.0; //  [mm] from PDG
+// X0 air = 36.66/1.204E-3 = 303.9 m
+#define X0_Air 304200.0
 
-  // X0 Kapton =  40.56 / 1.42 = 28.56 cm
-  double X0_Kapton =  285.6; // mm
+// X0 Kapton =  40.56 / 1.42 = 28.56 cm
+#define X0_Kapton  285.6
 
-  // X0 PCB (FR4)
-  // http://personalpages.to.infn.it/~tosello/EngMeet/ITSmat/SDD/SDD_G10FR4.html
-  double X0_PCB = 167.608; // mm
+// X0 PCB (FR4)
+// http://personalpages.to.infn.it/~tosello/EngMeet/ITSmat/SDD/SDD_G10FR4.html
+#define X0_PCB 167.608
 
-}
+#endif /* MATERIAL_H */
