52e6c3f46d23fee9ecf6a65a2af6adb7808bb037
Simon Spannagel <simon.spannagel@desy.de>
2016-01-29 11:24:52 +0100

    Add intrinsic resoltuion for Y and adapt X according to TB measurements (Thesis Simon)

--- utils/constants.h
+++ utils/constants.h
@@ -5,9 +5,15 @@
 #define beam_energy 0.250
 
 // Resolution analog planes in [um]
-#define resolution_analog 30e-3
+#define resolution_analog 33e-3
+
+// Resolution analog planes in [um]
+#define resolution_analog_y 51e-3
 
 // Resolution digital planes in [um]
-#define resolution_digital 30e-3
+#define resolution_digital 33e-3
+
+// Resolution digital planes in [um]
+#define resolution_digital_y 51e-3
 
 #endif /* CONST_H */
