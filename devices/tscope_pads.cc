719a7531db8c4f030fc762a2c1aeb90734695f22
Simon Spannagel <simon.spannagel@desy.de>
2016-01-29 11:25:17 +0100

    Pad: also calculate track resolution at pads in Y

--- devices/tscope_pads.cc
+++ devices/tscope_pads.cc
@@ -44,6 +44,7 @@ int main(int argc, char* argv[]) {
   
   //----------------------------------------------------------------------------
   // Build the trajectory through the telescope device:
+  LOG(logRESULT) << "Four-plane tracking:";
 
   plane pl0(0,analog_plane,true,resolution_analog);
   plane pl1(20.32,analog_plane,true,resolution_analog);
@@ -63,19 +64,38 @@ int main(int argc, char* argv[]) {
   planes.push_back(pl3);
 
   telescope mytel(planes);
-  LOG(logRESULT) << "Four-plane tracking:";
-  LOG(logRESULT) << "Track resolution at PAD1: " << mytel.getResolution(2);
-  LOG(logRESULT) << "Track resolution at PAD2: " << mytel.getResolution(3);
+  LOG(logRESULT) << "Track resolution (X) at PAD1: " << mytel.getResolution(2);
+  LOG(logRESULT) << "Track resolution (X) at PAD2: " << mytel.getResolution(3);
+
+
+    //----------------------------------------------------------------------------
+  // Build the trajectory through the telescope device:
+
+  plane ypl0(0,analog_plane,true,resolution_analog_y);
+  plane ypl1(20.32,analog_plane,true,resolution_analog_y);
+
+  plane ypad1(32,diamond_pad,false);
+  plane ypad2(51,diamond_pad,false);
+    
+  plane ypl2(81.28,analog_plane,true,resolution_analog_y);
+  plane ypl3(101.6,analog_plane,true,resolution_analog_y);
+
+  std::vector<plane> yplanes;
+  yplanes.push_back(ypl0);
+  yplanes.push_back(ypl1);
+  yplanes.push_back(ypad1);
+  yplanes.push_back(ypad2);
+  yplanes.push_back(ypl2);
+  yplanes.push_back(ypl3);
+
+  telescope ymytel(yplanes);
+  LOG(logRESULT) << "Track resolution (Y) at PAD1: " << ymytel.getResolution(2);
+  LOG(logRESULT) << "Track resolution (Y) at PAD2: " << ymytel.getResolution(3);
 
 
   //----------------------------------------------------------------------------
   // Build the trajectory through the telescope device:
-
-  plane apl0(0,analog_plane,true,resolution_analog);
-  plane apl1(20.32,analog_plane,true,resolution_analog);
-
-  plane apad1(32,diamond_pad,false);
-  plane apad2(51,diamond_pad,false);
+  LOG(logRESULT) << "Two-plane tracking:";
 
   std::vector<plane> aplanes;
   aplanes.push_back(pl0);
@@ -84,9 +104,21 @@ int main(int argc, char* argv[]) {
   aplanes.push_back(pad2);
 
   telescope amytel(aplanes);
-  LOG(logRESULT) << "Two-plane tracking:";
-  LOG(logRESULT) << "Track resolution at PAD1: " << amytel.getResolution(2);
-  LOG(logRESULT) << "Track resolution at PAD2: " << amytel.getResolution(3);
-  
+  LOG(logRESULT) << "Track resolution (X) at PAD1: " << amytel.getResolution(2);
+  LOG(logRESULT) << "Track resolution (X) at PAD2: " << amytel.getResolution(3);
+
+  //----------------------------------------------------------------------------
+  // Build the trajectory through the telescope device:
+
+  std::vector<plane> yaplanes;
+  yaplanes.push_back(ypl0);
+  yaplanes.push_back(ypl1);
+  yaplanes.push_back(ypad1);
+  yaplanes.push_back(ypad2);
+
+  telescope yamytel(yaplanes);
+  LOG(logRESULT) << "Track resolution (Y) at PAD1: " << yamytel.getResolution(2);
+  LOG(logRESULT) << "Track resolution (Y) at PAD2: " << yamytel.getResolution(3);
+
   return 0;
 }
