c312447fc4091ad99da478b8afcff06ce013851f
Simon Spannagel <simon.spannagel@desy.de>
2016-02-06 12:54:41 +0100

    Lower logging level to DEBUG

--- telescope/assembly.cc
+++ telescope/assembly.cc
@@ -50,27 +50,27 @@ telescope::telescope(std::vector<gblsim::plane> planes) :
   pl++;
   // Store plane label:
   m_listOfLabels.push_back(m_listOfPoints.size());
-  LOG(logINFO) << "Added first plane at " << arclength;
+  LOG(logDEBUG) << "Added first plane at " << arclength;
 
   // All planes except first:
   for(pl; pl != planes.end(); pl++) {
     // Let's first add the air:
     double plane_distance = pl->m_position - oldpos;
-    LOG(logDEBUG) << "Distance to next plane: " << plane_distance;
+    LOG(logDEBUG2) << "Distance to next plane: " << plane_distance;
     
     // Propagate [mm] from 0 to 0.21 = 0.5 - 1/sqrt(12)
     double distance = 0.21 * plane_distance; arclength += distance;
     // Add air:
     m_listOfPoints.push_back(getPoint(distance,getScatterer(beam_energy,0.5*plane_distance/X0_Air)));
-    LOG(logDEBUG2) << "Added air scat at " << arclength;
+    LOG(logDEBUG3) << "Added air scat at " << arclength;
     // Propagate [mm] 0.58 = from 0.21 to 0.79 = 0.5 + 1/sqrt(12)
     distance = 0.58 * plane_distance; arclength += distance;
     // Factor 0.5 for the air as it is split into two scatterers:
     m_listOfPoints.push_back(getPoint(distance,getScatterer(beam_energy,0.5*plane_distance/X0_Air)));
-    LOG(logDEBUG2) << "Added air scat at " << arclength;
+    LOG(logDEBUG3) << "Added air scat at " << arclength;
     // Propagate [mm] from 0 to 0.21 = 0.5 - 1/sqrt(12)
     distance = 0.21 * plane_distance; arclength += distance;
-    LOG(logINFO) << "Added air scatterers.";
+    LOG(logDEBUG) << "Added air scatterers.";
     
     if(pl->m_measurement) {
       m_listOfPoints.push_back(getPoint(distance,pl->m_resolution,getScatterer(beam_energy,pl->m_materialbudget)));
@@ -82,16 +82,16 @@ telescope::telescope(std::vector<gblsim::plane> planes) :
     oldpos = pl->m_position;
     // Store plane label:
     m_listOfLabels.push_back(m_listOfPoints.size());
-    LOG(logINFO) << "Added plane at " << arclength;
+    LOG(logDEBUG) << "Added plane at " << arclength;
   }
 
-  LOG(logINFO) << "Finished building trajectory.";
+  LOG(logDEBUG) << "Finished building trajectory.";
 }
 
 GblTrajectory telescope::getTrajectory() {
 
   GblTrajectory traj(m_listOfPoints, 0);
-  IFLOG(logDEBUG) { traj.printPoints(); }
+  IFLOG(logDEBUG2) { traj.printPoints(); }
   return traj;
 }
 
@@ -103,8 +103,8 @@ double telescope::getResolution(int plane) {
   int ndf;
 
   tr.fit(c2, ndf, lw);
-  LOG(logDEBUG) << " Fit: Chi2=" << c2 << ", Ndf=" << ndf << ", lostWeight=" << lw;
-  IFLOG(logDEBUG) { tr.printTrajectory(); }
+  LOG(logDEBUG2) << " Fit: Chi2=" << c2 << ", Ndf=" << ndf << ", lostWeight=" << lw;
+  IFLOG(logDEBUG2) { tr.printTrajectory(); }
 
   TVectorD aCorr(5);
   TMatrixDSym aCov(5);
@@ -119,6 +119,6 @@ double telescope::getResolution(int plane) {
 void telescope::printLabels() {
 
   for(size_t l = 0; l < m_listOfLabels.size(); l++) {
-    LOG(logINFO) << "Plane " << l << " label " << m_listOfLabels.at(l);
+    LOG(logDEBUG) << "Plane " << l << " label " << m_listOfLabels.at(l);
   }
 }
