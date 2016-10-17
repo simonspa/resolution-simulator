9a8c88e235bdb9a40eff69db6bc859b93880b377
Simon Spannagel <simon.spannagel@desy.de>
2016-10-17 14:08:59 +0200

    Update FindGBL cmake module

--- cmake/FindGBL.cmake
+++ cmake/FindGBL.cmake
@@ -1,4 +1,4 @@
-# - Try to find pxarCore used in native reader library for exporting RAW to pxarCore format needed for reconstruction
+# - Try to find General Broken Lines library for uncertainty propagation
 # Once done this will define
 #  GBL_FOUND - System has GBL
 #  GBL_INCLUDE_DIR - The GBL main include directories
@@ -14,18 +14,11 @@ find_library(GBL_LIBRARY NAMES GBL
 
 MESSAGE(STATUS "GBL: ${GBL_INCLUDE_DIR} ${GBL_LIBRARY}")
 
-#include(FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set GBL_FOUND to TRUE
 # if all listed variables are TRUE
-#find_package_handle_standard_args(pxarCore
-#  REQUIRED_VARS GBL_LIBRARY GBL_API_INCLUDE_DIR GBL_UTILS_INCLUDE_DIR PXAR_UTIL_INCLUDE_DIR)
-
-IF(GBL_LIBRARY AND GBL_INCLUDE_DIR)
-   SET(GBL_FOUND TRUE)
-   MESSAGE(STATUS "Found GBL library and headers.")
-ELSE(GBL_LIBRARY AND GBL_INCLUDE_DIR)
-  SET(GBL_FOUND FALSE)
-  MESSAGE(FATAL_ERROR "Could not find GBL library and headers. Please set $GBLPATH.")
-ENDIF(GBL_LIBRARY AND GBL_INCLUDE_DIR)
+find_package_handle_standard_args(GBL 
+				  REQUIRED_VARS GBL_LIBRARY GBL_INCLUDE_DIR 
+				  FAIL_MESSAGE "Could not find GBL library and headers. Please set the $GBLPATH environment variable.")
 
 mark_as_advanced(GBL_LIBRARY GBL_INCLUDE_DIR)
