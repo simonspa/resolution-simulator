simonspa/resolution-simulator

commit 0db2d68d6684d40ebe4572e3be5a6d4bbad02c0d

Move to Eigen3 for GBL calls, compatible with GBL >= v2.0

telescope/assembly.cc
@@ -141,8 +141,8 @@ telescope::telescope(std::vector<gblsim::plane> planes, double beam_energy, doub
     if(pl->m_measurement) {
       gbl::GblPoint point(getPoint(distance,pl->m_resolution,getScatterer(beam_energy,pl->m_materialbudget,total_materialbudget)));
       //m_listOfPoints.push_back(getPoint(distance,pl->m_resolution,getScatterer(beam_energy,pl->m_materialbudget,total_materialbudget)));
-      TMatrixD addDer(2,4);
-      addDer.Zero();
+      Eigen::Matrix<double, 2, 3> addDer;
+      addDer.setZero();
       if(arcDUT > 0) {
         addDer(0,0) = (arclength - (arcDUT + size/sqrt(12))); // First scatterer in target
 	addDer(1,1) = (arclength - (arcDUT + size/sqrt(12))); //
@@ -223,8 +223,8 @@ std::pair<double,double> telescope::getResolutionXY(int plane) const {
   LOG(logDEBUG2) << " Fit: Chi2=" << c2 << ", Ndf=" << ndf << ", lostWeight=" << lw;
   IFLOG(logDEBUG2) { tr.printTrajectory(); }
 
-  TVectorD aCorr(m_parameter);
-  TMatrixDSym aCov(m_parameter);
+  Eigen::VectorXd aCorr(m_parameter);
+  Eigen::MatrixXd aCov(m_parameter, m_parameter);
 
   // Get resolution at position of the DUT:
   if(plane < m_listOfLabels.size()) {
@@ -249,8 +249,8 @@ std::pair<double,double> telescope::getKinkResolutionXY(int plane) const {
   LOG(logDEBUG2) << " Fit: Chi2=" << c2 << ", Ndf=" << ndf << ", lostWeight=" << lw;
   IFLOG(logDEBUG2) { tr.printTrajectory(); }
 
-  TVectorD aCorr(m_parameter);
-  TMatrixDSym aCov(m_parameter);
+  Eigen::VectorXd aCorr(m_parameter);
+  Eigen::MatrixXd aCov(m_parameter, m_parameter);
 
   // Get resolution at position of the DUT:
   if(plane < m_listOfLabels.size()) {

telescope/assembly.h
@@ -35,7 +35,7 @@ namespace gblsim {
 	  double size);
 
     bool m_measurement;
-    TVectorD m_resolution;
+    Eigen::Vector2d m_resolution;
     bool m_scatterer;
     double m_materialbudget;
     double m_position;

telescope/propagate.cc
@@ -1,16 +1,16 @@
 #include "propagate.h"
 
-TMatrixD gblsim::Jac5(double ds) {
+gbl::Matrix5d gblsim::Jac5(double ds) {
   /* 
      straight line, no B-field
      track = 
      q/p, x', y', x, y
      0,   1,  2,  3, 4
   */
-  TMatrixD jac(5, 5);
-  jac.UnitMatrix();
-  jac[3][1] = ds; // x = xp * ds
-  jac[4][2] = ds; // y = yp * ds
+  gbl::Matrix5d jac;
+  jac.setIdentity();
+  jac(3, 1) = ds; // x = xp * ds
+  jac(4, 2) = ds; // y = yp * ds
   return jac;
 }
 
@@ -23,64 +23,60 @@ double gblsim::getTheta(double energy, double radlength, double total_radlength)
   return (0.0136*sqrt(radlength)/energy*(1+0.038*log(total_radlength)));
 }
 
-TVectorD gblsim::getScatterer(double energy, double radlength, double total_radlength) {
-  TVectorD scat(2);
+Eigen::Vector2d gblsim::getScatterer(double energy, double radlength, double total_radlength) {
+  Eigen::Vector2d scat;
 
   double theta = getTheta(energy,radlength,total_radlength);
-  scat[0] = 1.0 / (theta*theta);
-  scat[1] = 1.0 / (theta*theta);
+  scat << 1.0 / (theta*theta), 1.0 / (theta*theta);
 
   return scat;
 }
 
 // construct a GblPoint with a scatterer and a measurement
-gbl::GblPoint gblsim::getPoint(double dz, const TVectorD& res, const TVectorD& wscat) {
+gbl::GblPoint gblsim::getPoint(double dz, const Eigen::Vector2d& res, const Eigen::Vector2d& wscat) {
 
   // Propagate:
-  TMatrixD jacPointToPoint = Jac5(dz);
+  auto jacPointToPoint = Jac5(dz);
   gbl::GblPoint point(jacPointToPoint);
 
   // Add scatterer:
-  TVectorD scat(2);
-  scat.Zero(); // mean is zero
+  Eigen::Vector2d scat(0., 0.);
   point.addScatterer(scat, wscat);
   
   // Add measurement:
   // measurement = residual
-  TVectorD meas(2);
-  meas.Zero(); // ideal
+  Eigen::Vector2d meas;
+  meas.setZero(); // ideal
   // Precision = 1/resolution^2
-  TVectorD measPrec(2);
-  measPrec[0] = 1.0 / res[0] / res[0];
-  measPrec[1] = 1.0 / res[1] / res[1];
+  Eigen::Vector2d measPrec;
+  measPrec << 1.0 / res[0] / res[0], 1.0 / res[1] / res[1];
 
   // measurement plane == propagation plane
-  TMatrixD proL2m(2,2);
-  proL2m.UnitMatrix();
+  Eigen::Matrix2d proL2m;
+  proL2m.setIdentity();
   point.addMeasurement(proL2m, meas, measPrec);
 
   return point;
 }
 
-gbl::GblPoint gblsim::getPoint(double dz, double res, const TVectorD& wscat)
+gbl::GblPoint gblsim::getPoint(double dz, double res, const Eigen::Vector2d& wscat)
 {
   
-  TVectorD res2(2);
-  res2[0] = res;
-  res2[1] = res;
+  Eigen::Vector2d res2;
+  res2 << res, res;
   return getPoint(dz, res2, wscat);
 }
 
 // construct a GblPoint with only a scatterer
-gbl::GblPoint gblsim::getPoint(double dz, const TVectorD& wscat) {
+gbl::GblPoint gblsim::getPoint(double dz, const Eigen::Vector2d& wscat) {
 
   // Propagate:
-  TMatrixD jacPointToPoint = Jac5(dz);
+  auto jacPointToPoint = Jac5(dz);
   gbl::GblPoint point(jacPointToPoint);
 
   // Add scatterer:
-  TVectorD scat(2);
-  scat.Zero(); // mean is zero
+  Eigen::Vector2d scat;
+  scat.setZero(); // mean is zero
   point.addScatterer(scat, wscat);
 
   return point;
@@ -89,7 +85,7 @@ gbl::GblPoint gblsim::getPoint(double dz, const TVectorD& wscat) {
 gbl::GblPoint gblsim::getMarker(double dz) {
 
   // Propagate:
-  TMatrixD jacPointToPoint = Jac5(dz);
+  auto jacPointToPoint = Jac5(dz);
   gbl::GblPoint point(jacPointToPoint);
 
   return point;

telescope/propagate.h
@@ -1,14 +1,16 @@
 #include "TMatrixD.h"
+#include "TVectorD.h"
 #include "GblTrajectory.h"
+#include "GblData.h"
 
 namespace gblsim {
   
-  TMatrixD Jac5(double ds);
+  gbl::Matrix5d Jac5(double ds);
   double getTheta(double energy, double radlength, double total_radlength);
-  TVectorD getScatterer(double energy, double radlength, double total_radlength);
-  gbl::GblPoint getPoint(double dz, double res, const TVectorD& wscat);
-  gbl::GblPoint getPoint(double dz, const TVectorD& res, const TVectorD& wscat);
-  gbl::GblPoint getPoint(double dz, const TVectorD& wscat);
+  Eigen::Vector2d getScatterer(double energy, double radlength, double total_radlength);
+  gbl::GblPoint getPoint(double dz, double res, const Eigen::Vector2d& wscat);
+  gbl::GblPoint getPoint(double dz, const Eigen::Vector2d& res, const Eigen::Vector2d& wscat);
+  gbl::GblPoint getPoint(double dz, const Eigen::Vector2d& wscat);
   gbl::GblPoint getMarker(double dz);
 
 }

commit a58369431c6579fadf4d7f641078ff3bb0953458

Fix bug introduced in 0db2d68d6684d40ebe4572e3be5a6d4bbad02c0d

telescope/assembly.cc
@@ -97,7 +97,7 @@ telescope::telescope(std::vector<gblsim::plane> planes, double beam_energy, doub
         double plane_distance = pl->m_position - oldpos;
         LOG(TRACE) << "Distance to next plane: " << plane_distance;
         double distance = 0;
-        double size = NAN;
+        double size;
 
         // Check if a volume scatterer with radiation length != 0 has been defined:
         if(m_volumeMaterial > 0.0) {
@@ -132,13 +132,13 @@ telescope::telescope(std::vector<gblsim::plane> planes, double beam_energy, doub
             gbl::GblPoint point(
                 getPoint(distance, pl->m_resolution, getScatterer(beam_energy, pl->m_materialbudget, total_materialbudget)));
             // m_listOfPoints.push_back(getPoint(distance,pl->m_resolution,getScatterer(beam_energy,pl->m_materialbudget,total_materialbudget)));
-            Eigen::Matrix<double, 2, 3> addDer;
+            Eigen::Matrix<double, 2, 4> addDer;
             addDer.setZero();
             if(arcDUT > 0) {
                 addDer(0, 0) = (arclength - (arcDUT + size / sqrt(12))); // First scatterer in target
                 addDer(1, 1) = (arclength - (arcDUT + size / sqrt(12))); //
                 addDer(0, 2) = (arclength - (arcDUT - size / sqrt(12))); // second scatterer in target
-                addDer(1, 2) = (arclength - (arcDUT - size / sqrt(12))); //
+                addDer(1, 3) = (arclength - (arcDUT - size / sqrt(12))); //
                 LOG(DEBUG) << " size = " << size
                            << " lever arm left DUT-point = " << (arclength - (arcDUT + size / sqrt(12)))
                            << " and lever arm right DUT-point = " << (arclength - (arcDUT - size / sqrt(12)));
@@ -207,8 +207,8 @@ std::pair<double, double> telescope::getResolutionXY(size_t plane) const {
 
     GblTrajectory tr = getTrajectory();
 
-    double c2 = NAN, lw = NAN;
-    int ndf = 0;
+    double c2, lw;
+    int ndf;
 
     tr.fit(c2, ndf, lw);
     LOG(TRACE) << " Fit: Chi2=" << c2 << ", Ndf=" << ndf << ", lostWeight=" << lw;
@@ -232,8 +232,8 @@ std::pair<double, double> telescope::getKinkResolutionXY(size_t plane) const {
 
     GblTrajectory tr = getTrajectory();
 
-    double c2 = NAN, lw = NAN;
-    int ndf = 0;
+    double c2, lw;
+    int ndf;
 
     tr.fit(c2, ndf, lw);
     LOG(TRACE) << " Fit: Chi2=" << c2 << ", Ndf=" << ndf << ", lostWeight=" << lw;